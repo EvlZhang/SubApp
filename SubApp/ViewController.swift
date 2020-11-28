bb2050fda0e7ae2e3c3f6105058ff62aef6d107e
Yuhui Zhang <yz4317@nyu.edu>
2020-11-28 16:45:21 -0500

    Initial Commit

---
+++ SubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubApp
+//
+//  Created by Evelyn Zhang on 11/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
