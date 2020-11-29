1416716208e9d27af6828814c3c6c82a23a8c868
Yuhui Zhang <yz4317@nyu.edu>
2020-11-28 20:45:44 -0500

    Added CoreData

---
+++ SubApp/Data Model/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  SubApp
+//
+//  Created by Evelyn Zhang on 11/28/20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
